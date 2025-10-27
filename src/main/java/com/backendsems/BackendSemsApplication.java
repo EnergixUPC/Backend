9e894496be8b61131b9a836a62cef1f709d217c1
krxxg <u202312629@upc.edu.pe>
2025-10-27 12:18:06 -0500

    Add project structure

---
+++ src/main/java/com/backendsems/BackendSemsApplication.java
@@ -0,0 +1,13 @@
+package com.backendsems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSemsApplication.class, args);
+    }
+
+}
