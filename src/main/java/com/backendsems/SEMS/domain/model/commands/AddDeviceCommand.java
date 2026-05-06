4311e4cef470357d125fb7fbd2a68197255a1798
Joan Teves <u202117303@upc.edu.pe>
2026-05-06 18:52:01 -0500

    refactor(domain): remover type y lastActive de AddDeviceCommand

--- src/main/java/com/backendsems/SEMS/domain/model/commands/AddDeviceCommand.java
+++ src/main/java/com/backendsems/SEMS/domain/model/commands/AddDeviceCommand.java
@@ -6,10 +6,8 @@ package com.backendsems.SEMS.domain.model.commands;
 public record AddDeviceCommand(
         String name,
         String category,
-        String type,
         String status,
-        String lastActivity,
         String location,
-        boolean active
+        Long userId
 ) {
 }
