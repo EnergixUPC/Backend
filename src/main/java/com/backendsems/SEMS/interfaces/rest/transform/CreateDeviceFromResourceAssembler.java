7f9b218e07984e47de5fc636f2879f92c50c58b5
Joan Teves <u202117303@upc.edu.pe>
2026-05-06 18:53:31 -0500

    refactor(api): actualizar CreateDeviceFromResourceAssembler para omitir type y lastActive

--- src/main/java/com/backendsems/SEMS/interfaces/rest/transform/CreateDeviceFromResourceAssembler.java
+++ src/main/java/com/backendsems/SEMS/interfaces/rest/transform/CreateDeviceFromResourceAssembler.java
@@ -3,26 +3,14 @@ package com.backendsems.SEMS.interfaces.rest.transform;
 import com.backendsems.SEMS.domain.model.commands.AddDeviceCommand;
 import com.backendsems.SEMS.interfaces.rest.resources.CreateDeviceResource;
 
-/**
- * CreateDeviceFromResourceAssembler
- * Ensamblador para convertir CreateDeviceResource a AddDeviceCommand.
- */
 public class CreateDeviceFromResourceAssembler {
-
-    /**
-     * Convierte un CreateDeviceResource a AddDeviceCommand.
-     * @param resource El recurso de creación.
-     * @return AddDeviceCommand.
-     */
-    public static AddDeviceCommand toCommand(CreateDeviceResource resource) {
+    public static AddDeviceCommand toCommandFromResource(CreateDeviceResource resource) {
         return new AddDeviceCommand(
                 resource.name(),
                 resource.category(),
-                resource.type(),
                 resource.status(),
-                resource.lastActivity(),
                 resource.location(),
-                resource.active()
+                resource.userId()
         );
     }
 }
