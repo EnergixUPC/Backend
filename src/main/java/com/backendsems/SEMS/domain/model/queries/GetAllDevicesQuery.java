cb7b0f43dcd7cb4fac7d64bf4e2b1bdd7ac91e3a
JonatanFD <u20211b995@upc.edu.pe>
2026-05-11 00:01:07 -0500

    feat: se agrega filtro por user id en endpoint GetAllDevices

--- src/main/java/com/backendsems/SEMS/domain/model/queries/GetAllDevicesQuery.java
+++ src/main/java/com/backendsems/SEMS/domain/model/queries/GetAllDevicesQuery.java
@@ -6,5 +6,4 @@ import com.backendsems.SEMS.domain.model.valueobjects.UserId;
  * GetAllDevicesQuery
  * Query para obtener todos los dispositivos.
  */
-public record GetAllDevicesQuery() {
-}
+public record GetAllDevicesQuery(UserId userId) {}
